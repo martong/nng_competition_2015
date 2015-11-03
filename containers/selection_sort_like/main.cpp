6668cd206d6ff01e97b633374b7ed9e6bc2d2104
Gabor Marton <gabor.marton@prezi.com>
2015-11-03 21:57:04 +0100

    Remove unecessary reversals

--- containers/selection_sort_like/main.cpp
+++ containers/selection_sort_like/main.cpp
@@ -4,7 +4,6 @@
 #include <deque>
 #include <vector>
 #include <unordered_map>
-#include <set>
 #include <tuple>
 #include <cassert>
 
@@ -24,51 +23,15 @@
  */
 
 using Reversal = std::tuple<int, int>;
-using Exch = std::tuple<int, int>;
 
-using PalindromInBetween = std::set<Exch>;
-
-// Transform an exchange to at most two reversals.
 using Reversals = std::vector<Reversal>;
-Reversals map(const Exch& e, const PalindromInBetween& palindromInbetween) {
-    int x = std::get<0>(e);
-    int y = std::get<1>(e);
-    assert(x <= y);
-    if (x == y) return {};
-    if (x >= y - 2) return {{x, y}};
-    if (palindromInbetween.count(e)) return {{x, y}};
-    Reversals rev;
-    rev.push_back({x, y});
-    rev.push_back({x + 1, y - 1});
-    return rev;
-}
 
 using Result = std::vector<Reversal>;
-using ExchResult = std::vector<Exch>;
-// Transform all the exchanges to reversals
-Result map(const ExchResult& er, const PalindromInBetween& palindromInbetween) {
-    Result result;
-    for (const auto& exch : er) {
-        auto reversals = map(exch, palindromInbetween);
-        for (const auto& reversal : reversals) {
-            result.push_back(reversal);
-        }
-    }
-    return result;
-}
 
 std::ostream& operator<<(std::ostream& os, const Result& r) {
     os << r.size() << "\n";
     for (const auto& rev : r) {
         os << std::get<0>(rev) << " " << std::get<1>(rev) << "\n";
-    }
-    return os;
-}
-
-std::ostream& operator<<(std::ostream& os, const PalindromInBetween& ps) {
-    os << ps.size() << "\n";
-    for (const auto& p : ps) {
-        os << std::get<0>(p) << " " << std::get<1>(p) << "\n";
     }
     return os;
 }
@@ -96,46 +59,12 @@ void test_makePositionMap() {
     }
 }
 
-// Returns true if [b, e) is a palindrom in a.
-bool palindrom(const std::string& a, int b, int e) {
-    assert(b <= e);
-    int N = e - b;
-    for (int i = b; i < b + N / 2; ++i) {
-        if (a[i] != a[e - (i - b) - 1]) return false;
-    }
-    //std::cout << "Palindrom " << b << " " << e << "\n";
-    return true;
-};
-
-void test_palindrom() {
-    ASSERT(palindrom("a", 0, 1));
-    ASSERT(palindrom("aa", 0, 2));
-    ASSERT(palindrom("aaa", 0, 3));
-    ASSERT(palindrom("aba", 0, 3));
-    ASSERT(palindrom("abba", 0, 4));
-    ASSERT(palindrom("abbba", 0, 5));
-    ASSERT(palindrom("xiabbajy", 2, 6));
-
-    ASSERT(!palindrom("ab", 0, 2));
-    ASSERT(!palindrom("abab", 0, 4));
-}
-
-std::tuple<ExchResult, PalindromInBetween> exchSolve(
-    std::string actual, const std::string& desired) {
-
-    ExchResult result;
-    PalindromInBetween palindromInbetween;
-
-    auto exch = [&result, &palindromInbetween](std::string& a, int x, int y) {
+Result exchSolve(std::string actual, const std::string desired) {
+    Result result;
+    auto exch = [&result](std::string& a, int x, int y) {
         if (x == y) return;
-
-        if (palindrom(a, x + 1, y)) {
-            palindromInbetween.insert(Exch{x, y});
-        }
-
-        std::swap(a[x], a[y]);
-        //std::cout << "exch " << x << "," << y << std::endl;
-        result.push_back(Exch{x, y});
+        std::reverse(a.begin() + x, a.begin() + y + 1);
+        result.push_back({x, y});
     };
 
     auto pm = makePositionMap(desired);
@@ -157,30 +86,22 @@ std::tuple<ExchResult, PalindromInBetween> exchSolve(
         // Actualize the position map
         pm[actual[i]].pop_front();
     }
-    return {result, palindromInbetween};
+    return result;
 }
 
 void test_exchSolve() {
     {
-        auto er = std::get<0>(exchSolve("abc", "cab"));
-        ASSERT((er == ExchResult{{0, 2}, {1, 2}}));
+        auto er = exchSolve("abc", "cab");
+        ASSERT((er == Result{{0, 2}, {1, 2}}));
     }
     {
-        auto er = std::get<0>(exchSolve("abccb", "cabbc"));
-        ASSERT((er == ExchResult{{0, 2}, {1, 2}, {3, 4}}));
-    }
-    {
-        PalindromInBetween p;
-        ExchResult r;
-        std::tie(r, p) = exchSolve("xiabbajy", "xjabbaiy");
-        ASSERT((r == ExchResult{{1, 6}}));
-        ASSERT((p == PalindromInBetween{{1, 6}}));
+        auto er = exchSolve("abccb", "cabbc");
+        ASSERT((er == Result{{0, 2}, {1, 2}, {3, 4}}));
     }
 }
 
 int test() {
     test_makePositionMap();
-    test_palindrom();
     test_exchSolve();
     std::cout << "OK\n";
     return 0;
@@ -202,11 +123,8 @@ int main(int argc, char** argv) {
     } else
         std::cerr << "Unable to open file";
 
-    PalindromInBetween p;
-    ExchResult r;
-    std::tie(r, p) = exchSolve(actual, desired);
-    auto result = map(r, p);
+    auto result = exchSolve(actual, desired);
     std::cout << result;
-    //std::cout << p;
     return 0;
 }
+
