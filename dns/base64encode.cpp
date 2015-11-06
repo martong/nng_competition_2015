ba178680f7e36fa4d69af512b5579c5a88e8b75b
Peter Szabados <kangirigungi@gmail.com>
2015-11-06 21:35:19 +0100

    Add escape

--- dns/base64encode.cpp
+++ dns/base64encode.cpp
@@ -1,7 +1,17 @@
 #include "base64.hpp"
 
 #include <iostream>
+#include <algorithm>
 
 int main() {
+    constexpr int bufferSize = 10000;
+    char buffer[bufferSize];
+    std::string s;
+    while (std::cin.read(buffer, bufferSize).good()) {
+        std::copy(buffer, buffer + std::cin.gcount(), std::back_inserter(s));
+    }
 
+    std::cout << base64_encode(reinterpret_cast<const unsigned char*>(s.c_str()),
+            s.size());
+    return 0;
 }
--- dns/escape.cpp
+++ dns/escape.cpp
@@ -1,12 +1,56 @@
 #include <iostream>
+#include <iomanip>
 
 int main() {
-    char c;
-    while (std::cin >> c) {
-        if (c < 32) {
-            std::cout << '\\' << std::oct << (int)c << std::dec;
-        } else {
-            std::cout << c;
+    std::cout << R"(#include <iostream>
+int main() {
+    std::cout << ")";
+    unsigned char c;
+    bool wasNull = false;
+    while (std::cin.get(reinterpret_cast<char&>(c)).good()) {
+        switch (c) {
+        case '\0':
+            std::cout << "\\0";
+            break;
+        case '\a':
+            std::cout << "\\a";
+            break;
+        case '\b':
+            std::cout << "\\b";
+            break;
+        case '\f':
+            std::cout << "\\f";
+            break;
+        case '\n':
+            std::cout << "\\n";
+            break;
+        case '\r':
+            std::cout << "\\r";
+            break;
+        case '\t':
+            std::cout << "\\t";
+            break;
+        case '\v':
+            std::cout << "\\v";
+            break;
+        case '\\':
+            std::cout << "\\\\";
+            break;
+        case '"':
+            std::cout << "\\\"";
+            break;
+        default:
+            if (c < 0x20 || (c >= 0x7f && c < 0xa0)) {
+                std::cout << "\\" << std::oct << std::setw(3) <<
+                        std::setfill('0') << (int)c;
+            } else if (wasNull && c >= '0' && c <= '9') {
+                std::cout << "00" << c;
+            } else {
+                std::cout << c;
+            }
         }
+        wasNull = c == '\0';
     }
+    std::cout << "\";\n}\n";
+    return 0;
 }
