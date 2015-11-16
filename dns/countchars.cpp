fb3409232d3be69054545cb9dba97936bbaad2c7
Peter Szabados <kangirigungi@gmail.com>
2015-11-16 19:54:06 +0100

    Correct countchars

--- dns/countchars.cpp
+++ dns/countchars.cpp
@@ -7,14 +7,14 @@ int main() {
     while (std::cin.get(reinterpret_cast<char&>(c)).good()) {
         map[c]++;
     }
-    std::map<int, char> map2;
+    std::multimap<int, char> map2;
 
     for (const auto& element : map) {
         //std::cout << (int)element.first << ": " << element.second << "\n";
         map2.emplace(element.second, element.first);
     }
     for (const auto& element : map2) {
-        std::cout << element.first << ": " << (int)element.second << "\n";
+        std::cout << (int)element.second << ": " << element.first << "\n";
     }
     return 0;
 }
