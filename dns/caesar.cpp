0b540db24ecf2a59e8f3958d54bdaecc842585c2
Peter Szabados <kangirigungi@gmail.com>
2015-11-16 19:27:44 +0100

    Add caesar encoding to minimize escaping

--- dns/caesar.cpp
+++ dns/caesar.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <boost/lexical_cast.hpp>
 
-int main() {
+int main(int, char* argv[]) {
     char c;
+    int i = boost::lexical_cast<int>(argv[1]);
     while (std::cin.get(reinterpret_cast<char&>(c)).good()) {
-        std::cout << (char)(c - 74);
+        std::cout << (char)(c - i);
     }
     return 0;
 }
