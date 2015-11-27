99f977202b4662e3734b19a2ef0fa92638dedc15
András Németh <andrasnemeth@users.noreply.github.com>
2015-11-27 12:19:55 +0100

    Fix direction string.

--- FINAL/Client/Response.hpp
+++ FINAL/Client/Response.hpp
@@ -13,7 +13,7 @@ enum class Dir {
 };
 
 std::ostream& operator<<(std::ostream& out, const Dir& direction){
-    static std::vector<std::string> directionStrings{"left", "right", "top",
+    static std::vector<std::string> directionStrings{"left", "right", "up",
                 "down"};
     return out << directionStrings[static_cast<int>(direction)];
 }
