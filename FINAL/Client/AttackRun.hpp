bb46313ad4fc5a04fcbd5fa05ba9ffaf4f37298b
Péter Szabados <kangirigungi@gmail.com>
2015-11-27 12:00:11 +0100

    Run away from enemy better

--- FINAL/Client/AttackRun.hpp
+++ FINAL/Client/AttackRun.hpp
@@ -2,6 +2,7 @@
 #define CLIENT_ATTACKRUN_HPP
 
 #include "Table.hpp"
+#include "PointRange.hpp"
 #include <cstdlib>
 #include <set>
 #include <assert.h>
@@ -10,11 +11,20 @@ Point attackRunOverride(const Table& table, Point p, Point d) {
     assert(std::abs(p.x - d.x) + std::abs(p.y - d.y) == 1);
     assert(table[p]);
     assert(!table[p]->enemy);
+    Soldier mySoldier = table[p]->soldier;
     auto destinations = {p - p10, p + p10, p - p01, p + p01};
     std::set<Point> doNotGo;
+    for (Point pp : arrayRange(table)) {
+        if (table[pp] && table[pp]->enemy && less(mySoldier, table[pp]->soldier)) {
+            doNotGo.insert(pp + p10);
+            doNotGo.insert(pp + p01);
+            doNotGo.insert(pp - p10);
+            doNotGo.insert(pp - p01);
+        }
+    }
     for (auto destination : destinations) {
         if (table[destination] && table[destination]->enemy) {
-            if (less(table[destination]->soldier, table[p]->soldier)) {
+            if (less(table[destination]->soldier, mySoldier)) {
                 return destination;
             }
             doNotGo.insert(destination);
